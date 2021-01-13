2e36497b381f722e0c0a949cc446c983c9221ae5
BraggsHerman <braggsherman@wustl.edu>
2021-01-12 20:27:15 -0600

    third commit, 830

--- src/App.js
+++ src/App.js
@@ -1,11 +1,21 @@
+import React from 'react';
+import CardEditor from './CardEditor'
 
+class App extends React.Component {
 
-function App() {
-  return (
-    <div className="App">
-   Testing
-    </div>
-  );
+  constructor(props) {
+    super(props);
+    this.state = {
+      cards: [
+        {front: 'front1', back: 'back1'}
+        ,{front: 'front2', back: 'back2'}
+      ]
+    }
+  }
+  render(){
+    return <CardEditor cards = {this.state.cards}/>;
+  }
+  
 }
 
 export default App;
---
+++ src/CardEditor.js
@@ -0,0 +1,23 @@
+import React from 'react';
+
+class CardEditor extends React.Component {
+    render(){
+        return(
+            <div>
+                <h2>Card Editor</h2>
+                <table>
+                    <thead>
+                        <tr>
+                            <th>Front</th>
+                            <th>Back</th>
+                            <th>Delete</th>
+                        </tr>
+                    </thead>
+                </table>
+            </div>    
+        );
+    }
+
+}
+
+export default CardEditor;
